b7071dd3dbb4a6d0658a00b0479b5dc345d5f877
Bernardo Arcari Denkvitts <Bernardoarcari@gmail.com>
2025-07-15 21:28:40 -0300

    tempo utils

--- epidemiologia/src/main/java/com/tcc/epidemiologia/utils/TempoUtils.java
+++ epidemiologia/src/main/java/com/tcc/epidemiologia/utils/TempoUtils.java
@@ -1,11 +1,10 @@
 package com.tcc.epidemiologia.utils;
 
-import java.util.Date;
 import java.util.concurrent.TimeUnit;
 import java.util.function.LongSupplier;
 
 public class TempoUtils {
-    public static LongSupplier clock = System::currentTimeMillis; // por padrão
+    public static LongSupplier clock = System::currentTimeMillis;
 
     public static void setClock(LongSupplier c) {
         clock = c;
