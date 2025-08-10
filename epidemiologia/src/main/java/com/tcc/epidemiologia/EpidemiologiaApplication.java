ce6890e2df424c05d9f2bb2be2e3e3ab714c2492
Bernardo Arcari Denkvitts <Bernardoarcari@gmail.com>
2025-08-10 14:50:10 -0300

    anottation faltante, removido campo desnecessário do yaml

--- epidemiologia/src/main/java/com/tcc/epidemiologia/EpidemiologiaApplication.java
+++ epidemiologia/src/main/java/com/tcc/epidemiologia/EpidemiologiaApplication.java
@@ -1,23 +1,19 @@
 package com.tcc.epidemiologia;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
 @EnableAsync
 @EnableRetry
-public class EpidemiologiaApplication implements CommandLineRunner {
+@EnableScheduling
+public class EpidemiologiaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EpidemiologiaApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-
 	}
 }
